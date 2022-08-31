cc4136b26bdccd2d70a558bc07bc42064f7be815
hjali7 <88620768+hjali7@users.noreply.github.com>
2022-08-31 11:28:41 +0430

    finished project

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let searchBar =document.querySelector('.search-bar')
+let button = document.querySelector('button')
+
 let weather = {
     "APIkey" : "751c7946b80d85f448db9972e0bc711f",
     fetchWeather : function(city) {
@@ -11,12 +14,30 @@ let weather = {
         const{ description ,icon  } = data.weather[0] ;
         const{temp , humidity} = data.main ; 
         const{ speed } = data.wind ;
-        console.log(name,description,icon ,temp ,humidity,speed)
         document.querySelector('.city').innerHTML  = 'weather in ' + name;
         document.querySelector('.temp').innerHTML = temp + '°C'
         document.querySelector('.humidity').innerHTML  = 'humidity: ' + humidity + '%'
         document.querySelector('.wind').innerHTML = 'wind speed : ' + speed + 'km/h'
         document.querySelector('.description').innerHTML  = description
         document.querySelector('.icon').src = 'https://openweathermap.org/img/wn/'+ icon + '.png'
-    }
+        document.querySelector('body').style.backgroundImage = `url(https://source.unsplash.com/1600x900/?` + name  +` )`
+    },
+    search: function() {
+        this.fetchWeather(searchBar.value)
+        document.querySelector('.weather').classList.remove('loading')
+    }    
 }
+
+button.addEventListener('click',()=>{
+    weather.search()
+    searchBar.value = ''
+})
+
+searchBar.addEventListener('keyup',(e)=>{
+    if(e.key === 'Enter'){
+        weather.search()
+        searchBar.value = ''
+    }
+})
+
+weather.fetchWeather('tehran')
